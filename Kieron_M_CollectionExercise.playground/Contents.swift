e11ca81788f25e4eb571e8d19931ac0036e798c2
KieronMoore <67976013+KieronMoore@users.noreply.github.com>
2020-07-15 14:10:11 -0500

    Add files via upload

---
+++ Kieron_M_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// Creating an array
+var collectionOfVideoGames: [String] = ["NBA 2k20" , "NBA Live 19" , "Madden 20" , "FIFA 20" , "Rainbow Six Seige" , "COD Modern Warfare" , "Mortal Combat 11", "Uncharted 4" , "NBA 2k19"]
+
+collectionOfVideoGames.count
+
+collectionOfVideoGames.append("NBA 2k18")
+
+collectionOfVideoGames.count
+
+for item in collectionOfVideoGames{
+    print(item)
+}
